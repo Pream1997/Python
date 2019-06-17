9966313944391a8eadbb05e2a7d051a34443004f
Pream1997 <42028723+Pream1997@users.noreply.github.com>
2019-06-17 12:36:27 +0530

    Update B3.py

--- B3.py
+++ B3.py
@@ -1,8 +1,8 @@
 n=input()
-vowels=['a','A','e','E','i','I','o','O','u','U']
+vowel=['a','A','e','E','i','I','o','O','u','U']
 Consonant=['b','B','c','C','d','D','f','F','j','J','k','K','l','L','m','M','n','N','p','P','q','Q','r','R','s','S','t','T','v','V','w','W','x','X','y','Y','z','Z']
-if n in vowels:
-    print("Vowels")
+if n in vowel:
+    print("Vowel")
 elif n in Consonant:
     print("Consonant")
 else:
